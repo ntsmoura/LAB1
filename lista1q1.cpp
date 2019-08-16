7537d11aa08405a805c9ff77a0af3c5a3e81d3e2
ntsmoura <natloc215@hotmail.com>
2019-08-16 14:42:41 -0300

    16/08/19

--- lista1q1.cpp
+++ lista1q1.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 int main(){
   vector <int> vetor;
-  int num;
-  while(cin >> num)
+  int num,n;
+  cin >> n;
+  for(int i = 0; i <n;i++){
+    cin >> num;
     vetor.push_back(num);
+  }
+  int caixa;
+  cin >> caixa;
+  sort(vetor.begin(),vetor.end());
+  int ls,li,m,saida=-1;
+  ls = n-1;
+  li = 0;
+  while(li <= ls){
+      m = (ls+li)/2;
+      if(vetor[m]==caixa){ saida=caixa; break;}
+      else if(caixa > vetor[m]) li = m+1;
+      else ls = m-1;
+  }
+  cout << saida << endl;
 }
