70ad84fd5bcfe216148f6e9ecb453555a545400f
ntsmoura <natloc215@hotmail.com>
2019-08-18 16:45:27 -0300

    q3

--- lista1q3.cpp
+++ lista1q3.cpp
@@ -2,5 +2,15 @@
 #include<vector>
 using namespace std;
 int main(){
-    
+    int n;
+    cin >> n;
+    vector <string> times;
+    string ntime;
+    for (int i = 0; i<=n;i++){
+        getline(cin,ntime);
+        times.push_back(ntime);
+    }
+     for (int k = 1; k<=(n/2);k++){
+        cout << times[k] << " X " << times[n-k+1] << endl; 
+    }
 }
